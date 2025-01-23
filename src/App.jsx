edb619a106c0abdffa813150f76a2acf75895362
MaicolCode <paucarjhon07@outlook.com>
2025-01-23 15:56:20 -0500

    Se aplica a app la funcion de layer para la pagina principal de carga de las paginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { Outlet, Route, Routes } from 'react-router-dom'
 import Footer from './components/Footer'
 import Nav from './components/Nav'
+import HomePage from './pages/HomePage'
 
 function App() {
   return (
     <main>
       <Nav />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+      </Routes>
+      <div>
+        <Outlet />
+      </div>
       <Footer />
     </main>
   )
