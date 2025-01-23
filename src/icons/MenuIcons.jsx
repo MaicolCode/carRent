a68ffabe6424969f3d61612d464549b68ee0f389
MaicolCode <paucarjhon07@outlook.com>
2025-01-23 16:59:08 -0500

    Creacion de los nuevos iconos en este caso flechas a usar en la pagina

--- src/icons/MenuIcons.jsx
+++ src/icons/MenuIcons.jsx
@@ -85,3 +85,63 @@ export function SearchIcon() {
     </svg>
   )
 }
+
+export function ArrowRight() {
+  return (
+    <svg
+      width='14'
+      height='14'
+      viewBox='0 0 14 14'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M7.00038 9.79969C6.59204 9.79969 6.18371 9.64219 5.87454 9.33303L2.07121 5.52969C1.90204 5.36053 1.90204 5.08053 2.07121 4.91136C2.24038 4.74219 2.52038 4.74219 2.68954 4.91136L6.49288 8.71469C6.77288 8.99469 7.22788 8.99469 7.50788 8.71469L11.3112 4.91136C11.4804 4.74219 11.7604 4.74219 11.9295 4.91136C12.0987 5.08053 12.0987 5.36053 11.9295 5.52969L8.12621 9.33303C7.81705 9.64219 7.40871 9.79969 7.00038 9.79969Z'
+        fill='#1A202C'
+        stroke='#1A202C'
+        strokeWidth='0.5'
+      />
+    </svg>
+  )
+}
+
+export function Arrows() {
+  return (
+    <svg
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M7.16051 3.83577L7.16051 17.4536'
+        stroke='white'
+        strokeWidth='1.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M3.08273 7.93188L7.1605 3.83522L11.2383 7.93188'
+        stroke='white'
+        strokeWidth='1.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M17.0887 20.1671L17.0887 6.54934'
+        stroke='white'
+        strokeWidth='1.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M21.1665 16.071L17.0887 20.1677L13.0109 16.071'
+        stroke='white'
+        strokeWidth='1.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  )
+}
