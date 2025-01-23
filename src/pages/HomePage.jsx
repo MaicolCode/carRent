1f54695d7b6d19eba20e6b923561b1f559ad00da
MaicolCode <paucarjhon07@outlook.com>
2025-01-23 16:59:23 -0500

    Creacion de las cartas de agenda de la pagina web

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,10 @@
+/* eslint-disable react/prop-types */
+import { ArrowRight, Arrows } from '../icons/MenuIcons'
+
 export default function HomePage() {
   return (
-    <section>
-      <section className='flex justify-between gap-7 py-8 px-8'>
+    <section className='bg-gray-100'>
+      <section className='flex justify-between gap-7 pt-8 px-8'>
         <div className=' relative w-[640px] h-[320px] rounded-lg px-[20px] py-5 bg-[#54A6FF]'>
           <h2 className='text-[27px] text-white font-medium w-[45%]'>
             The Best Platform for Car Rental
@@ -45,8 +48,63 @@ export default function HomePage() {
           />
         </div>
       </section>
-      <section></section>
-      <section></section>
+      <section className='flex justify-between items-center gap-7 py-10 px-8'>
+        <CardAgend title='Pick - Up' primary='#3563E9' secondary='#3563E9' />
+        <button className='bg-[#3563E9] w-[60px] h-[60px] flex justify-center items-center rounded-[5px] hover:bg-opacity-90 transition-all duration-500 ease-in-out'>
+          <Arrows />
+        </button>
+        <CardAgend title='Drop - Off' primary='#54A6FF' secondary='#54A6FF' />
+      </section>
+      <section className='pb-10 px-8'>
+        <div className='flex justify-between items-center gap-7 px-6 text-sm font-medium'>
+          <a href='' className='text-gray-400'>
+            Popular Cars
+          </a>
+          <a href='' className='text-[#3563E9]'>
+            View All
+          </a>
+        </div>
+      </section>
+    </section>
+  )
+}
+
+function CardAgend({ title, primary, secondary }) {
+  return (
+    <section className='bg-white w-[45%] py-6 px-10 rounded-lg'>
+      <div className='flex items-center gap-3'>
+        <span
+          className={`w-[15px] h-[15px] rounded-full bg-[${primary}] bg-opacity-40 flex justify-center items-center animate-pulse`}
+        >
+          <span
+            className={`w-[7px] h-[7px] rounded-full bg-[${secondary}] `}
+          ></span>
+        </span>
+        <h2 className='font-medium text-sm'>{title}</h2>
+      </div>
+      <section className='flex gap-3 mt-4'>
+        <div className='flex flex-col gap-2'>
+          <h3 className='font-bold text-sm'>Locations</h3>
+          <button className='flex w-[120px] justify-between'>
+            <span className='text-xs text-gray-400'>Select your city</span>
+            <ArrowRight />
+          </button>
+        </div>
+        <div className='flex flex-col gap-2 border-x border-gray-200 px-5'>
+          <h3 className='font-bold text-sm'>Date</h3>
+          <button className='flex w-[120px] justify-between'>
+            <span className='text-xs text-gray-400'>Select your date</span>
+            <ArrowRight />
+          </button>
+        </div>
+        <div className='flex flex-col gap-2'>
+          <h3 className='font-bold text-sm'>Time</h3>
+          <button className='flex w-[120px] justify-between'>
+            <span className='text-xs text-gray-400'>Select your time</span>
+            <ArrowRight />
+          </button>
+        </div>
+      </section>
     </section>
   )
 }
