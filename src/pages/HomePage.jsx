836367de732b1c8be10c016a432235c9e6ede64b
MaicolCode <paucarjhon07@outlook.com>
2025-02-10 17:28:17 -0500

    Componente principal para la visualizacion de autos

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,13 @@
 /* eslint-disable react/prop-types */
+import { useEffect, useState } from 'react'
 import { ArrowRight, Arrows } from '../icons/MenuIcons'
+import {
+  GasStation,
+  Like,
+  LikePinted,
+  SteeringWheel,
+  TwoUsers
+} from '../icons/CardCars'
 
 export default function HomePage() {
   return (
@@ -64,6 +72,7 @@ export default function HomePage() {
             View All
           </a>
         </div>
+        <PopularCars />
       </section>
     </section>
   )
@@ -106,5 +115,87 @@ function CardAgend({ title, primary, secondary }) {
         </div>
       </section>
     </section>
+  )
+}
+
+function PopularCars() {
+  const [cars, setCars] = useState([])
+  const [liked, setLiked] = useState(false)
+
+  useEffect(() => {
+    fetch('http://localhost:3000/cars')
+      .then((res) => res.json())
+      .then((data) => setCars(data))
+  }, [])
+
+  const handleLike = (e) => {
+    e.preventDefault()
+    console.log(e.target)
+    setLiked(!liked)
+  }
+
+  console.log(cars)
+
+  return (
+    <div className='grid grid-cols-4 gap-10 mt-7'>
+      {cars.map((car) => {
+        return (
+          <section
+            key={car._id}
+            className='w-[280px] h-[360px] p-5 rounded-lg bg-white'
+          >
+            <div className='flex justify-between '>
+              <div className='flex flex-col gap-1'>
+                <h2 className='text-lg font-semibold'>{car.brand}</h2>
+                <span className='text-gray-400 font-semibold text-xs'>
+                  {car.type}
+                </span>
+              </div>
+              <span onClick={handleLike} className='mt-2'>
+                {liked ? <Like /> : <LikePinted />}
+              </span>
+            </div>
+
+            <div className='relative flex justify-center items-center h-[190px]'>
+              <div className='absolute bottom-0 w-[200px] h-[110px] bg-gradient-to-b from-transparent to-white'></div>
+              <img
+                className='flex justify-center items-center'
+                src={`http://localhost:3000${car.exteriorImage}`}
+                alt={car.brand}
+                width={200}
+              />
+            </div>
+            <section className='flex justify-between text-xs font-medium text-slate-400'>
+              <div className='flex items-center gap-1'>
+                <GasStation />
+                <span>{car.gasoline}LT</span>
+              </div>
+              <div className='flex items-center gap-1'>
+                <SteeringWheel />
+                <span>{car.steering}</span>
+              </div>
+              <div className='flex items-center gap-2'>
+                <TwoUsers />
+                <span>{car.capacity} People</span>
+              </div>
+            </section>
+            <section className='flex justify-between items-center w-auto my-4'>
+              <p>
+                <strong className='text-lg'>${car.pricePerDay}.00/</strong>
+                <span className='ml-1 text-xs font-semibold text-slate-400'>
+                  day
+                </span>
+              </p>
+              <a
+                href=''
+                className='bg-[#3563E9] text-white text-sm  flex justify-center items-center rounded-[5px] w-[110px] h-[40px] hover:bg-opacity-90 transition-all'
+              >
+                Rent Now
+              </a>
+            </section>
+          </section>
+        )
+      })}
+    </div>
   )
 }
