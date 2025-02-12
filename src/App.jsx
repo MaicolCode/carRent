5a6705761df672dc8d77c887b95bf2380af06607
MaicolCode <paucarjhon07@outlook.com>
2025-02-11 22:47:15 -0500

    Agregando ruta para mostrar al lista de todos los automoviles

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { Outlet, Route, Routes } from 'react-router-dom'
 import Footer from './components/Footer'
 import Nav from './components/Nav'
 import HomePage from './pages/HomePage'
+import AllCarsPage from './pages/AllCarsPage'
 
 function App() {
   return (
     <main>
       <Nav />
       <Routes>
-        <Route path='/' element={<HomePage />} />
+        <Route index element={<HomePage />} />
+        <Route path='/all-cars' element={<AllCarsPage />} />
       </Routes>
       <div>
         <Outlet />
