49c8561bdfc063bfb72bfd04f71d8e20d3c811d0
MaicolCode <paucarjhon07@outlook.com>
2025-02-17 19:18:13 -0500

    Edición en la forma de enviar datos de los autos en cada una de las rutas de información y renta

--- src/components/CardCar.jsx
+++ src/components/CardCar.jsx
@@ -6,27 +6,35 @@ import {
   SteeringWheel,
   TwoUsers
 } from '../icons/CardCars'
+import { Link } from 'react-router-dom'
 
-export default function CardCarPresentation({ car }) {
+export default function CardCarPresentation({ car, actionClick }) {
   const [liked, setLiked] = useState(true)
   const handleLike = () => {
     setLiked(!liked)
   }
+
   return (
     <section
-      key={car._id}
-      className='w-[280px] h-[360px] p-5 rounded-lg bg-white'
+      onClick={() => {
+        actionClick(car.id)
+      }}
+      key={car.id}
+      className='w-[280px] h-[360px] p-5 rounded-lg bg-white cursor-pointer'
     >
       <div className='flex justify-between '>
         <div className='flex flex-col gap-1'>
-          <h2 className='text-lg font-semibold'>{car.brand}</h2>
+          <h2 className='text-lg font-semibold'>{car.name}</h2>
           <span className='text-gray-400 font-semibold text-xs'>
-            {car.type}
+            {car.category}
           </span>
         </div>
         <span
-          onClick={handleLike}
-          className='flex justify-center items-center h-[20px] w-[20px]'
+          onClick={(e) => {
+            e.stopPropagation()
+            handleLike()
+          }}
+          className='flex justify-center items-center h-[20px] w-[20px] '
         >
           {liked ? <Like /> : <LikePinted />}
         </span>
@@ -36,36 +44,40 @@ export default function CardCarPresentation({ car }) {
         <div className='absolute bottom-0 w-[200px] h-[110px] bg-gradient-to-b from-transparent to-white'></div>
         <img
           className='flex justify-center items-center'
-          src={`http://localhost:3000${car.exteriorImage}`}
-          alt={car.brand}
+          src={`https://morent-website.vercel.app${car.image}`}
+          alt={car.name}
           width={200}
         />
       </div>
       <section className='flex justify-between text-xs font-medium text-slate-400'>
         <div className='flex items-center gap-1'>
           <GasStation />
-          <span>{car.gasoline}LT</span>
+          <span>{car.gas}LT</span>
         </div>
         <div className='flex items-center gap-1'>
           <SteeringWheel />
-          <span>{car.steering}</span>
+          <span>{car.gear}</span>
         </div>
         <div className='flex items-center gap-2'>
           <TwoUsers />
-          <span>{car.capacity} People</span>
+          <span>{car.capacity}</span>
         </div>
       </section>
       <section className='flex justify-between items-center w-auto my-4'>
         <p>
-          <strong className='text-lg'>${car.pricePerDay}.00/</strong>
+          <strong className='text-lg'>${car.price}.00/</strong>
           <span className='ml-1 text-xs font-semibold text-slate-400'>day</span>
         </p>
-        <a
-          href=''
+        <Link
+          onClick={(e) => {
+            e.stopPropagation()
+          }}
+          to={`rent-car`}
+          state={{ car }}
           className='bg-[#3563E9] text-white text-sm  flex justify-center items-center rounded-[5px] w-[110px] h-[40px] hover:bg-opacity-90 transition-all'
         >
           Rent Now
-        </a>
+        </Link>
       </section>
     </section>
   )
