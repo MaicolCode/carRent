f0778d99a577244ea72a84d40547de0ca857ce8b
MaicolCode <paucarjhon07@outlook.com>
2025-03-27 20:09:54 -0500

    Refactor CarContext to optimize state management using useCallback and useMemo

--- src/contexts/car.jsx
+++ src/contexts/car.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from 'react'
+import { createContext, useCallback, useEffect, useMemo, useState } from 'react'
 import { useLocation, useNavigate, useParams } from 'react-router-dom'
 
 export const CarContext = createContext()
@@ -13,29 +13,34 @@ export function CarProvider({ children }) {
   useEffect(() => {
     fetch('https://morent-website.vercel.app/api/cars')
       .then((res) => res.json())
-      .then((data) => setCars(data))
-  }, [])
-
-  useEffect(() => {
-    if (!car) {
-      fetch('https://morent-website.vercel.app/api/cars')
-        .then((res) => res.json())
-        .then((data) => {
+      .then((data) => {
+        setCars(data)
+        if (!car) {
           const findCar = data.find((car) => car.id === parseInt(id))
           serCar(findCar)
-        })
-    }
-  }, [location.pathname])
+        }
+      })
+  }, [id, car])
 
-  const navigateToCar = (id) => {
-    navigate(`cars/${id}`, {
-      state: { car: cars.find((car) => car.id === id) }
-    })
-  }
+  const navigateToCar = useCallback(
+    (id) => {
+      navigate(`cars/${id}`, {
+        state: { car: cars.find((car) => car.id === id) }
+      })
+    },
+    [navigate, cars]
+  )
+
+  const contextValue = useMemo(
+    () => ({
+      cars,
+      car,
+      navigateToCar
+    }),
+    [cars, car, navigateToCar]
+  )
 
   return (
-    <CarContext.Provider value={{ cars, car, navigateToCar }}>
-      {children}
-    </CarContext.Provider>
+    <CarContext.Provider value={contextValue}>{children}</CarContext.Provider>
   )
 }
