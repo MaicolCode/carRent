0625d33e5c71e4f61beb7a806c82bd21f669cc17
MaicolCode <paucarjhon07@outlook.com>
2025-03-30 16:46:07 -0500

    Convert to responsive design content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,21 +3,21 @@ import { memo } from 'react'
 // eslint-disable-next-line react/display-name
 const Footer = memo(() => {
   return (
-    <footer className='p-16'>
-      <section className='flex justify-between py-14'>
-        <div>
-          <a href='/' className='font-bold text-3xl text-[#3563E9]'>
+    <footer className='p-4 md:p-16'>
+      <section className='flex flex-col md:flex-row justify-between py-8 md:py-14 gap-8 md:gap-0'>
+        <div className='text-center md:text-left'>
+          <a href='/' className='font-bold text-2xl md:text-3xl text-[#3563E9]'>
             MORENT
           </a>
-          <p className='text-gray-500 w-3/5 mt-5'>
+          <p className='text-gray-500 w-full md:w-3/5 mt-3 md:mt-5 text-sm md:text-base'>
             Our vision is to provide convenience and help increase your sales
             business.
           </p>
         </div>
-        <div className='flex gap-20'>
-          <div>
-            <h3 className='font-semibold text-xl'>About</h3>
-            <ul className='mt-10 flex flex-col gap-7 text-gray-500'>
+        <div className='flex flex-col md:flex-row gap-8 md:gap-20'>
+          <div className='text-center md:text-left'>
+            <h3 className='font-semibold text-lg md:text-xl'>About</h3>
+            <ul className='mt-4 md:mt-10 flex flex-col gap-4 md:gap-7 text-gray-500'>
               <li>
                 <a href=''>How it works</a>
               </li>
@@ -32,9 +32,9 @@ const Footer = memo(() => {
               </li>
             </ul>
           </div>
-          <div>
-            <h3 className='font-semibold text-xl'>Community</h3>
-            <ul className='mt-10 flex flex-col gap-7 text-gray-500'>
+          <div className='text-center md:text-left'>
+            <h3 className='font-semibold text-lg md:text-xl'>Community</h3>
+            <ul className='mt-4 md:mt-10 flex flex-col gap-4 md:gap-7 text-gray-500'>
               <li>
                 <a href=''>Events</a>
               </li>
@@ -49,9 +49,9 @@ const Footer = memo(() => {
               </li>
             </ul>
           </div>
-          <div>
-            <h3 className='font-semibold text-xl'>Socials</h3>
-            <ul className='mt-10 flex flex-col gap-7 text-gray-500'>
+          <div className='text-center md:text-left'>
+            <h3 className='font-semibold text-lg md:text-xl'>Socials</h3>
+            <ul className='mt-4 md:mt-10 flex flex-col gap-4 md:gap-7 text-gray-500'>
               <li>
                 <a href=''>Discord</a>
               </li>
@@ -69,9 +69,9 @@ const Footer = memo(() => {
         </div>
       </section>
       <hr />
-      <section className='flex justify-between py-14 text-sm font-semibold'>
-        <p>&copy; 2022 MORENT. All rights reserved.</p>
-        <div className='flex gap-20'>
+      <section className='flex flex-col md:flex-row justify-between py-6 md:py-14 text-xs md:text-sm font-semibold gap-4 md:gap-0'>
+        <p className='text-center md:text-left'>&copy; 2022 MORENT. All rights reserved.</p>
+        <div className='flex flex-col md:flex-row gap-4 md:gap-20 items-center'>
           <a href='#privacy'>Privacy & Policy</a>
           <a href='#terms'>Terms & Condition</a>
         </div>
