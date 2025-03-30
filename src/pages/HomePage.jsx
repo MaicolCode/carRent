a763c3dd94893776d094e86e4c9c1c6933a9ae83
MaicolCode <paucarjhon07@outlook.com>
2025-03-30 17:14:14 -0500

    Updating the design to responsive design in mobile window view

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,12 +10,12 @@ export default function HomePage() {
   }, [])
   return (
     <section className='bg-gray-100'>
-      <section className='flex justify-between gap-7 pt-8 px-8'>
-        <div className=' relative w-[640px] h-[320px] rounded-lg px-[20px] py-5 bg-[#54A6FF]'>
-          <h2 className='text-[27px] text-white font-medium w-[45%]'>
+      <section className='flex md:flex-row flex-col justify-between gap-7 pt-8 px-4 md:px-8'>
+        <div className=' relative md:w-[640px] w-full h-[320px] rounded-lg px-[20px] py-5 bg-[#54A6FF]'>
+          <h2 className='text-[27px] text-white font-medium md:w-3/6'>
             The Best Platform for Car Rental
           </h2>
-          <p className='text-sm w-3/6 text-white mt-4'>
+          <p className='text-sm md:w-3/6 text-white mt-4'>
             Ease of doing a car rental safely and reliably. Of course at a low
             price.
           </p>
@@ -28,15 +28,14 @@ export default function HomePage() {
           <img
             src='/cars/car-010.svg'
             alt=''
-            width={380}
-            className='absolute right-[80px] bottom-2'
+            className='absolute w-[260px] md:w-[380px] md:right-[80px] right-[20px] bottom-2'
           />
-        </div>
-        <div className=' relative w-[640px] h-[320px] rounded-lg px-[20px] py-5 bg-[#3563E9]'>
-          <h2 className='text-[27px] text-white font-medium w-3/6'>
+        </div> 
+        <div className=' relative md:w-[640px] w-full h-[320px] rounded-lg px-[20px] py-5 bg-[#3563E9]'>
+          <h2 className='text-[27px] text-white font-medium md:w-3/6'>
             Easy way to rent a car at a low price
           </h2>
-          <p className='text-sm w-3/6 text-white mt-4'>
+          <p className='text-sm md:w-3/6 text-white mt-4'>
             Providing cheap car rental services and safe and comfortable
             facilities.
           </p>
@@ -49,8 +48,7 @@ export default function HomePage() {
           <img
             src='/cars/car-020.svg'
             alt=''
-            width={330}
-            className='absolute right-[90px] bottom-3'
+            className='absolute w-[260px] md:w-[380px] md:right-[90px] right-[20px] bottom-3'
           />
         </div>
       </section>
