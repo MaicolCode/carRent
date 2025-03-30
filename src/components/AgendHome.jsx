a299c54b93b96f7da37e232c3a8988a3fbe07fe1
MaicolCode <paucarjhon07@outlook.com>
2025-03-30 17:12:08 -0500

    Update agend to responsive design

--- src/components/AgendHome.jsx
+++ src/components/AgendHome.jsx
@@ -2,7 +2,7 @@ import { ArrowRight, Arrows } from '../icons/MenuIcons'
 
 export function AgendHome() {
   return (
-    <section className='flex justify-between items-center gap-7 py-10 px-8'>
+    <section className='flex md:flex-row flex-col justify-between items-center gap-7 py-10 px-4 md:px-8'>
       <CardAgend title='Pick - Up' primary='#3563E9' secondary='#3563E9' />
       <button className='bg-[#3563E9] w-[60px] h-[60px] flex justify-center items-center rounded-[5px] hover:bg-opacity-90 transition-all duration-500 ease-in-out'>
         <Arrows />
@@ -14,7 +14,7 @@ export function AgendHome() {
 
 function CardAgend({ title, primary, secondary }) {
   return (
-    <section className='bg-white w-auto py-6 px-10 rounded-lg'>
+    <section className='bg-white md:w-auto w-full py-6 px-4 md:px-10 rounded-lg'>
       <div className='flex items-center gap-3'>
         <span
           className={`w-[15px] h-[15px] rounded-full bg-[${primary}] bg-opacity-40 flex justify-center items-center animate-pulse`}
@@ -28,22 +28,22 @@ function CardAgend({ title, primary, secondary }) {
       <section className='flex gap-3 mt-4'>
         <div className='flex flex-col gap-2'>
           <h3 className='font-bold text-sm'>Locations</h3>
-          <button className='flex w-[110px] justify-between p-0'>
-            <span className='text-xs text-gray-400'>Select your city</span>
+          <button className='flex w-[95px] sm:w-[110px] justify-between p-0'>
+            <span className='text-[10px] md:text-xs text-gray-400'>Select your city</span>
             <ArrowRight />
           </button>
         </div>
-        <div className='flex flex-col gap-2 border-x border-gray-200 px-5'>
+        <div className='flex flex-col gap-2 border-x border-gray-200 px-2 md:px-5'>
           <h3 className='font-bold text-sm'>Date</h3>
-          <button className='flex w-[110px] justify-between'>
-            <span className='text-xs text-gray-400'>Select your date</span>
+          <button className='flex w-[95px] sm:w-[110px] justify-between'>
+            <span className='text-[10px] md:text-xs text-gray-400'>Select your date</span>
             <ArrowRight />
           </button>
         </div>
         <div className='flex flex-col gap-2'>
           <h3 className='font-bold text-sm'>Time</h3>
-          <button className='flex w-[110px] justify-between'>
-            <span className='text-xs text-gray-400'>Select your time</span>
+          <button className='flex w-[95px] sm:w-[110px] justify-between'>
+            <span className='text-[10px] md:text-xs text-gray-400'>Select your time</span>
             <ArrowRight />
           </button>
         </div>
