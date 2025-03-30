22b0364c2363b17c5c9e6efffec7d4f7dbf221d1
MaicolCode <paucarjhon07@outlook.com>
2025-03-30 16:40:15 -0500

    Update the contents of the agenda in the mobile window

--- src/components/AllCars.jsx
+++ src/components/AllCars.jsx
@@ -17,8 +17,8 @@ const AllCars = memo(({ cars }) => {
 
   return (
     <Suspense fallback={<h1>Loading...</h1>}>
-      <section className='w-full p-8 '>
-        <section className='relative flex justify-between items-center gap-10 w-full'>
+      <section className='w-full p-5 md:p-8 '>
+        <section className='relative flex md:flex-row flex-col justify-between items-center gap-10 w-full'>
           <CardAgend title='Pick - Up' primary='#3563E9' secondary='#3563E9' />
           <button className='absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] bg-[#3563E9] w-[55px] h-[55px] flex justify-center items-center rounded-lg hover:bg-opacity-90 transition-all duration-500 ease-in-out shadow-slate-400 shadow-[0_0_10px_2px]'>
             <Arrows />
@@ -55,10 +55,10 @@ const CarPresetation = memo(function CarPresetation({ car, actionClick }) {
 
 const CardAgend = memo(function CardAgend({ title, primary, secondary }) {
   return (
-    <section className='bg-white w-full py-6 px-5 rounded-lg'>
+    <section className='bg-white w-full py-6 px-3 md:px-5 rounded-lg'>
       <div className='flex items-center gap-3'>
         <span
-          className={`w-[15px] h-[15px] rounded-full bg-[${primary}] bg-opacity-40 flex `}
+          className={`w-[15px] h-[15px] rounded-full bg-[${primary}] bg-opacity-40 flex justify-center items-center`}
         >
           <span
             className={`w-[7px] h-[7px] rounded-full bg-[${secondary}] `}
@@ -66,15 +66,15 @@ const CardAgend = memo(function CardAgend({ title, primary, secondary }) {
         </span>
         <h2 className='font-medium text-sm'>{title}</h2>
       </div>
-      <section className='flex gap-3 mt-4'>
+      <section className='flex gap-1 md:gap-3 mt-4'>
         <div className='flex flex-col gap-2'>
           <h3 className='font-bold text-sm'>Locations</h3>
-          <button className='flex w-[100px] justify-between p-0'>
+          <button className='flex w-[100px] justify-between'>
             <span className='text-[10px] text-gray-400'>Select your city</span>
             <ArrowRight />
           </button>
         </div>
-        <div className='flex flex-col gap-2 border-x border-gray-200 px-5'>
+        <div className='flex flex-col gap-2 border-x border-gray-200 px-3'>
           <h3 className='font-bold text-sm'>Date</h3>
           <button className='flex w-[100px] justify-between'>
             <span className='text-[10px] text-gray-400'>Select your date</span>
